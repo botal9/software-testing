ca25aa0b98443581d464216db5fece64eba60726
Vitalii Ovechkin <ovechkinve@yandex-team.ru>
2020-10-21 00:10:35 +0300

    Finish sideblock

---
+++ client/src/components/comments/Comments.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserContext from '../app/UserContext'
+
+function Comments() {
+    return (
+        <div></div>
+    )
+}
+
+export default Comments;
--- client/src/components/sidebar/components/UserInfoBlock.js
+++ client/src/components/sidebar/components/UserInfoBlock.js
@@ -1,14 +1,22 @@
 import UserContext from '../../app/UserContext';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function UserInfoBlock() {
     return (
         <UserContext.Consumer>
             {value =>
                 <section className='UserInfoBlock'>
-                    <div className='Sidebar-block__header'>{value.user.login}</div>
-                    <div className='Sidebar-block__content'>
-
+                    <div className='UserInfoBlock__header'>
+                        <div className='avatar'></div>
+                        <div className='user'>
+                            <div className='user-login'>{value.user.login}</div>
+                            <div className='logout' onClick={() => value.logout()}>logout</div>
+                        </div>
+                    </div>
+                    <div className='UserInfoBlock__user-info-list'>
+                        <Link className='list-item posts' to='/Posts'>Posts</Link>
+                        <Link className='list-item comments' to='/Comments'>Comments</Link>
                     </div>
                 </section>
             }
