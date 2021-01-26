a180228fe6a98650c17ebfaf22ac93105a194aa8
Vitalii Ovechkin <ovechkinve@yandex-team.ru>
2021-01-27 00:54:57 +0300

    Set up Playwrite

---
+++ client/src/test/playwrite.test.js
@@ -0,0 +1,17 @@
+const playwright = require('playwright');
+
+describe('Playwrite tests', () => {
+    it('aaa', () => {
+        (async () => {
+            for (const browserType of ['chromium', 'firefox']) {
+                const browser = await playwright[browserType].launch();
+                const context = await browser.newContext();
+                const page = await context.newPage();
+                await page.goto('http://whatsmyuseragent.org/');
+                await page.screenshot({ path: `example-${browserType}.png` });
+                await browser.close();
+            }
+        })();
+    })
+})
+
